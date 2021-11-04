3bf0f8c755f0e30e1d3b19ba1e81926a5ecc325c
LAPTOP-OLIVER <jamols09@gmail.com>
2021-11-04 20:47:18 +0800

    feat: change created at date format

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,7 @@ class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'created_at' => "datetime:Y-m-d",
     ];
 
     public function scopeAdmin($query)
