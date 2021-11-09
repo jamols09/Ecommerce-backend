a886e32be607baf5fab9df8783130b04e6c2fa81
LAPTOP-OLIVER <jamols09@gmail.com>
2021-11-10 00:00:55 +0800

    refactor: transfer eloquent query to spatie query builder for table with pagination

--- app/Http/Backend/V1/Services/UserService.php
+++ app/Http/Backend/V1/Services/UserService.php
@@ -5,6 +5,7 @@ namespace App\Http\Backend\V1\Services;
 use Exception;
 use App\Http\Backend\V1\Repositories\UserRepository;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class UserService
@@ -28,48 +29,6 @@ class UserService
     }
 
     /**
-     * @param array $data
-     * @return paginated user
-     */
-    public function table($data)
-    {
-        $query = $data['u'] == 'a' ? User::query()->admin() : User::query()->client();
-
-        switch (strtolower($data['type'])) {
-            case 'username':
-                $query = $query->where('username', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'firstname':
-                $query = $query->where('first_name', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'middlename':
-                $query = $query->where('middle_name', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'lastname':
-                $query = $query->where('last_name', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'fullname':
-                $query = $query->orWhereRaw("concat(first_name, ' ', middle_name, ' ', last_name) like ?", ['%' . $data['q'] . '%']);
-            case 'created at':
-                $query = $query->where('created_at', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            default:
-                break;
-        }
-
-        if ($data['col']) {
-            $data['col'] = $data['col'] == 'Active' ? 'is_active' : $data['col'];
-            $query = $query->orderBy(str_replace(' ', '_', $data['col'] ?? 'id'), $data['order'] ?? 'asc');
-        }
-        
-        if ($data['row']) {
-            $query = $query->paginate($data['row'] ?? 1)->onEachSide(1);
-        }
-
-        return $query;
-    }
-
-    /**
      * Delete all selected id
      * 
      * @param array $data
@@ -77,10 +36,12 @@ class UserService
      */
     public function delete($data)
     {
-        foreach ($data['id'] as $value) {
-            $query = User::find($value)->delete();
-        }
-        return $query;
+        DB::transaction(function () use ($data) {
+            foreach ($data['id'] as $value) {
+                $query = User::find($value)->delete();
+            }
+            return $query;
+        });
     }
 
     /**
@@ -91,9 +52,11 @@ class UserService
      */
     public function status($data)
     {
-        foreach ($data['id'] as $value) {
-            $query = User::find($value)->update(['is_active' => $data['status'] == 'activate' ? 1 : 0]);
-        }
-        return $query;
+        DB::transaction(function () use ($data) {
+            foreach ($data['id'] as $value) {
+                $query = User::find($value)->update(['is_active' => $data['status'] == 'activate' ? 1 : 0]);
+            }
+            return $query;
+        });
     }
 }
