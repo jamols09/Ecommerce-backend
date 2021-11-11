9d7c5e6e534032b2b5b9cf21d1b7040e5461f145
LAPTOP-OLIVER <jamols09@gmail.com>
2021-11-11 12:57:12 +0800

    refactor:  change eloquent query to spatie query builder for table with pagination on Branch

--- app/Http/Backend/V1/Controller/BranchController.php
+++ app/Http/Backend/V1/Controller/BranchController.php
@@ -5,8 +5,10 @@ namespace App\Http\Backend\V1\Controller;
 use App\Http\Backend\V1\Services\BranchService;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Backend\BranchCreateRequest;
+use App\Models\Branch;
 use Illuminate\Http\Request;
 use Exception;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class BranchController extends Controller
 {
@@ -50,12 +52,34 @@ class BranchController extends Controller
     public function table(Request $request)
     {
         try {
-            $result['body'] = $this->branchService->table($request);
-        } catch (Exception $e) {
+            $result['body'] = QueryBuilder::for(Branch::class)
+                ->allowedFilters([
+                    'name',
+                    'code',
+                    'city',
+                    'barangay',
+                    'address_line_1',
+                    'telephone',
+                    'mobile',
+                ])
+                ->allowedSorts(
+                    'name',
+                    'code',
+                    'city',
+                    'barangay',
+                    'address_line_1',
+                    'telephone',
+                    'mobile',
+                    'is_active'
+                )
+                ->paginate(request()->query()['row'] ?? 10)
+                ->onEachSide(1);
+        } catch (\Exception $e) {
             $result = [
                 'error' => $e->getMessage(),
+                'status' => 500,
             ];
-            return response()->json($result,500);
+            return response()->json($result, 500);
         }
         return response()->json($result, 200);
     }
--- app/Http/Backend/V1/Services/BranchService.php
+++ app/Http/Backend/V1/Services/BranchService.php
@@ -25,52 +25,6 @@ class BranchService
 		return $this->branchRepository->dropdown();
 	}
 
-	public function table($data)
-    {
-        $query = Branch::query();
-
-        switch (strtolower($data['type'])) {
-            case 'name':
-                $query = $query->where('name', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'code':
-                $query = $query->where('code', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'city':
-                $query = $query->where('city', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'barangay':
-                $query = $query->where('barangay', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'line 1':
-                $query =  $query->where('address_line_1', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'telephone':
-                $query =  $query->where('telephone', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'mobile':
-                $query =  $query->where('mobile', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            case 'created at':
-                $query = $query->where('created_at', 'LIKE', '%' . $data['q'] . '%');
-                break;
-            default:
-                break;
-        }
-
-        if ($data['col']) {
-            //override values
-            $data['col'] = $data['col'] == 'Active' ? 'is_active' : $data['col'];
-            $query = $query->orderBy(str_replace(' ', '_', $data['col'] ?? 'id'), $data['order'] ?? 'asc');
-        }
-
-        if ($data['row']) {
-            $query = $query->paginate($data['row'] ?? 1)->onEachSide(1);
-        }
-
-        return $query;
-    }
-
     /**
      * Delete all selected id
      * @param array $data
