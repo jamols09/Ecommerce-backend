181b3d0f47eda5654d97c4ebc28472c9bd8ef6a0
LAPTOP-OLIVER <jamols09@gmail.com>
2022-01-23 17:12:45 +0800

    remove unused code

--- app/Http/Backend/V1/Repositories/BranchRepository.php
+++ app/Http/Backend/V1/Repositories/BranchRepository.php
@@ -3,7 +3,6 @@
 namespace App\Http\Backend\V1\Repositories;
 
 use App\Models\Branch;
-use App\Models\User;
 
 class BranchRepository
 {
@@ -19,21 +18,4 @@ class BranchRepository
         $this->branch = $branch;
     }
 
-    /**
-     * Create branch from admin
-     */
-    public function create($data)
-    {
-
-        return $this->branch::create($data);
-    }
-
-    /**
-     * Get all branches from dropdown
-     * @return App\Models\Branch ['id','name']
-     */
-    public function dropdown()
-    {
-        return $this->branch::orderBy('name')->get(['id', 'name']);
-    }
 }
